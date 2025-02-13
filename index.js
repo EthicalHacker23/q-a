f3341bd6b01695e4fd0131d735cd699736c459c4
HackForceCoder <milanalba@outlook.com>
2025-02-13 09:12:08 -0600

    website ready

---
+++ index.js
@@ -0,0 +1,14 @@
+function calculate(){
+    document.getElementById("result").innerText = "Result: " + 
+        `${(
+            parseFloat(document.getElementById("num1").value) + 
+            parseFloat(document.getElementById("num2").value)
+        )}`;
+}
+
+function resetFields(){
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").innerText = "Result: ";
+}
+
